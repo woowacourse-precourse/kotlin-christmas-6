2e7128f70f46c41f6af948f08499c8f46a2b8ad8
김준호 <kimjunho@gimjunhoui-MacBookAir.local>
2023-11-14 19:06:20 +0900

    1114

---
+++ src/main/kotlin/christmas/View.kt
@@ -0,0 +1,45 @@
+package christmas
+
+open class View {
+    var commingday:Int = 0
+
+    var YangsonisupCnt = 0
+    var TapasCnt = 0
+    var CizersaladCnt = 0
+    var TbonesteakCnt = 0
+    var BabequeripCnt = 0
+    var HasanmulpastaCnt = 0
+    var ChrismaspastaCnt = 0
+    var ChococakeCnt = 0
+    var IcecreamCnt = 0
+    var ZerocokeCnt = 0
+    var RedwineCnt = 0
+    var SyampainCnt = 0
+
+    var YangsonisupMoney = 6000
+    var TapasMoney = 5500
+    var CizersaladMoney = 8000
+    var TbonesteakMoney = 55000
+    var BabequeripMoney = 54000
+    var HasanmulpastaMoney = 35000
+    var ChrismaspastaMoney = 25000
+    var ChococakeMoney = 15000
+    var IcecreamMoney = 5000
+    var ZerocokeMoney = 3000
+    var RedwineMoney = 60000
+    var SyampainMoney = 25000
+
+    val YangsonisupStr = "양송이수프"
+    val TapasStr = "타파스"
+    val CizersaladStr = "시저샐러드"
+    val TbonesteakStr = "티본스테이크"
+    val BabequeripStr = "바비큐립"
+    val HasanmulpastaStr = "해산물파스타"
+    val ChrismaspastaStr = "크리스마스파스타"
+    val ChococakeStr = "초코케이크"
+    val IcecreamStr = "아이스크림"
+    val ZerocokeStr = "제로콜라"
+    val RedwineStr = "레드와인"
+    val SyampainStr = "샴페인"
+
+}
